8439b00bff5f569d836e29d2e3da4a2578fa494a
Ben Liang <benpkliang@gmail.com>
2014-03-02 12:40:22 -0800

    forgot this one

--- src/ca/ubc/cs310/gwt/healthybc/server/ClinicHours.java
+++ src/ca/ubc/cs310/gwt/healthybc/server/ClinicHours.java
@@ -3,6 +3,10 @@ package ca.ubc.cs310.gwt.healthybc.server;
 public class ClinicHours {
 	private String hours;
 	
+	public ClinicHours(String hours) {
+		this.hours = hours;
+	}
+	
 	public String getHours() { return hours; }
 	
 	@Override
