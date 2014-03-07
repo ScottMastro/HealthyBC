d9c8b0f9b1ee9b65e3a14d3cb10e1023b02a4923
Alex Tan <tanalex1@gmail.com>
2014-03-06 20:17:01 -0800

    Revert c8d058e..4ba9acd
    This rolls back to commit c8d058ea4e0fa310650957f0ba43d16d01b1565b.

--- src/ca/ubc/cs310/gwt/healthybc/client/HealthyBC.java
+++ src/ca/ubc/cs310/gwt/healthybc/client/HealthyBC.java
@@ -2,12 +2,8 @@ package ca.ubc.cs310.gwt.healthybc.client;
 
 import java.util.ArrayList;
 
-<<<<<<< HEAD
 import org.apache.http.conn.routing.RouteInfo.LayerType;
 
-=======
-import ca.ubc.cs310.gwt.healthybc.server.Location;
->>>>>>> maps
 import ca.ubc.cs310.gwt.healthybc.shared.FieldVerifier;
 
 import com.google.gwt.core.client.EntryPoint;
@@ -27,18 +23,7 @@ import com.google.gwt.maps.client.LoadApi.LoadLibrary;
 import com.google.gwt.maps.client.MapOptions;
 import com.google.gwt.maps.client.MapWidget;
 import com.google.gwt.maps.client.base.LatLng;
-<<<<<<< HEAD
 import com.google.gwt.user.cellview.client.CellTable;
-=======
-import com.google.gwt.maps.client.events.MapEvent;
-import com.google.gwt.maps.client.events.click.ClickMapEvent;
-import com.google.gwt.maps.client.events.click.ClickMapHandler;
-import com.google.gwt.maps.client.overlays.InfoWindow;
-import com.google.gwt.maps.client.overlays.InfoWindowOptions;
-import com.google.gwt.maps.client.overlays.Marker;
-import com.google.gwt.maps.client.overlays.MarkerOptions;
-import com.google.gwt.user.client.Event;
->>>>>>> maps
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
@@ -54,9 +39,6 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class HealthyBC implements EntryPoint {
-	
-	private MapWidget map;
-	
 	/**
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
@@ -116,59 +98,21 @@ public class HealthyBC implements EntryPoint {
 		options.setZoom(13);
 		options.setCenter(vanCity);
 		
-		map = new MapWidget(options);
-		map.setSize("1200px", "800px");
+		MapWidget map = new MapWidget(options);
+		map.setSize("500px", "500px");
 		map.getElement().setId("mapWidget");
 		
-<<<<<<< HEAD
 		layout.add(map);
 		layout.setWidgetLeftRight(map, 50, Unit.PCT, 0, Unit.PCT);
-=======
-		ArrayList<MapInfo> clinics = new ArrayList<MapInfo>();
-		clinics.add(new MapInfo("Test Clinic 1", LatLng.newInstance(49.265082, -123.244573)));
-		clinics.add(new MapInfo("Test Clinic 2", LatLng.newInstance(49.263671, -123.146184)));
-		clinics.add(new MapInfo("Test Clinic 3", LatLng.newInstance(48.42349, -123.366963)));
-		
-		displayClinics(map, clinics);
-		
-		RootLayoutPanel.get().add(map);
-		
->>>>>>> maps
 	}
 	
-	private void displayClinics(final MapWidget map, ArrayList<MapInfo> clinics) {
-		for (MapInfo clinic : clinics) {
-			MarkerOptions options = MarkerOptions.newInstance();
-			options.setMap(map);
-			options.setClickable(true);
-			options.setTitle(clinic.getName());
-			options.setPosition(clinic.getLatLng());
-
-			final Marker marker = Marker.newInstance(options);
-			
-			final String desc = clinic.getName();
-			
-			final InfoWindowOptions iwOptions = InfoWindowOptions.newInstance();
-			final InfoWindow infoWindow = InfoWindow.newInstance(iwOptions);
-			
-			ClickMapHandler handler = new ClickMapHandler() {
-				public void onEvent(ClickMapEvent e) {
-					System.out.println(desc);
-//					infoWindow.setContent("<span style=\"display:inline-block;height:80px;width:180px;\">" + desc + "</span>");
-					infoWindow.setContent("<div style=\"max-width:400px; line-height:normal; white-space:nowrap; overflow:auto;\">" + desc + "</div>");
-					infoWindow.open(map, marker);
-				} 
-			};
-			marker.addClickHandler(handler);
-		}
-	}
 	private void loadMapApi() {
 		boolean sensor = false;
 		
 		// Load libraries needed for maps
 	    ArrayList<LoadLibrary> loadLibraries = new ArrayList<LoadApi.LoadLibrary>();
 //	    loadLibraries.add(LoadLibrary.ADSENSE);
-	    loadLibraries.add(LoadLibrary.DRAWING);		// need this api to draw overlays/markers on the map
+	    loadLibraries.add(LoadLibrary.DRAWING);		// need this api to draw overlays on the map
 //	    loadLibraries.add(LoadLibrary.GEOMETRY);
 //	    loadLibraries.add(LoadLibrary.PANORAMIO);
 //	    loadLibraries.add(LoadLibrary.PLACES);
--- src/ca/ubc/cs310/gwt/healthybc/client/MapInfo.java
+++ src/ca/ubc/cs310/gwt/healthybc/client/MapInfo.java
@@ -2,8 +2,6 @@ package ca.ubc.cs310.gwt.healthybc.client;
 
 import java.io.Serializable;
 
-import com.google.gwt.maps.client.base.LatLng;
-
 import ca.ubc.cs310.gwt.healthybc.server.Location;
 
 public class MapInfo implements Serializable {
@@ -14,9 +12,8 @@ public class MapInfo implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private String name;
-	private LatLng loc;
+	private Location loc;
 	
-<<<<<<< HEAD
 	public MapInfo(String name, Location loc) {
 		this.name = name;
 		this.loc = loc;
@@ -24,22 +21,14 @@ public class MapInfo implements Serializable {
 	
 	public String getName() { return name; }
 	public Location getLocation() { return loc; }
-=======
-	public MapInfo(String string, LatLng loc) {
-		this.name = string;
-		this.loc = loc;
-	}
-	public String getName() { return this.name; }
-	public LatLng getLatLng() { return this.loc; }
->>>>>>> maps
 
 	public MapInfo setName(String n) {
-		this.name = n;
+		name = n;
 		return this;
 	}
 	
-	public MapInfo setLatLng(LatLng l) {
-		this.loc = l;
+	public MapInfo setLocation(Location l) {
+		loc = l;
 		return this;
 	}
 }
