d5c253f81e54428b9e877c55542a5e3441573d62
Dhananjay Bhaskar <w7d7@ugrad.cs.ubc.ca>
2014-03-20 01:12:51 -0700

    Created authentication servlet

---
+++ src/ca/ubc/cs310/gwt/healthybc/server/Login.java
@@ -0,0 +1,50 @@
+package ca.ubc.cs310.gwt.healthybc.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@SuppressWarnings("serial")
+public class Login extends HttpServlet
+{   
+	Logger logger = Logger.getLogger("uploadServletLogger");
+
+	
+	/**
+	 * Pushes the request to POST
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		doPost(request, response);
+	}
+	
+	/**
+	 * Authorizes the user
+	 * @return response: true if login valid, false otherwise
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		String resp = "";
+		
+		// TODO: check username and password
+		
+		resp = "success";	// test successful login
+		// resp = "fail";	// test failed login
+		
+		response.setContentType("text/html");
+
+		PrintWriter out = response.getWriter();
+		out.println(resp);
+		out.close();
+
+	}
+	
+}
