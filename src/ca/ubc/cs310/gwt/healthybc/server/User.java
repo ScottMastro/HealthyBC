59577e1e2a4c959e276a10e1dfd83141b17c8784
ScottMastro <scott-mastro@hotmail.com>
2014-04-02 18:46:23 -0700

    less warnings

--- src/ca/ubc/cs310/gwt/healthybc/server/User.java
+++ src/ca/ubc/cs310/gwt/healthybc/server/User.java
@@ -1,12 +1,8 @@
 package ca.ubc.cs310.gwt.healthybc.server;
  
-import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
  
 
 
